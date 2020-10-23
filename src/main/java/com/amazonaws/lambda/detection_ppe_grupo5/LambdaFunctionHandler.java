021c1ea00de77367621a96c1977778257f997f4c
Juan Diego Carvajal Castaño <juandiego.carvajal@hotmail.com>
2020-10-23 18:52:17 -0500

    Now the lambda function can send an email report from the PPE detection analysis

--- src/main/java/com/amazonaws/lambda/detection_ppe_grupo5/LambdaFunctionHandler.java
+++ src/main/java/com/amazonaws/lambda/detection_ppe_grupo5/LambdaFunctionHandler.java
@@ -2,6 +2,7 @@ package com.amazonaws.lambda.detection_ppe_grupo5;
 
 import java.util.List;
 
+import com.amazonaws.regions.Regions;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.amazonaws.services.lambda.runtime.events.S3Event;
@@ -20,137 +21,188 @@ import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import com.amazonaws.services.s3.model.GetObjectRequest;
 import com.amazonaws.services.s3.model.S3Object;
+import com.amazonaws.services.simpleemail.AmazonSimpleEmailService;
+import com.amazonaws.services.simpleemail.AmazonSimpleEmailServiceClientBuilder;
+import com.amazonaws.services.simpleemail.model.Body;
+import com.amazonaws.services.simpleemail.model.Content;
+import com.amazonaws.services.simpleemail.model.Destination;
+import com.amazonaws.services.simpleemail.model.Message;
+import com.amazonaws.services.simpleemail.model.SendEmailRequest;
 
 
 public class LambdaFunctionHandler implements RequestHandler<S3Event, String> {
 
-    private AmazonS3 s3 = AmazonS3ClientBuilder.standard().build();
+	private AmazonS3 s3 = AmazonS3ClientBuilder.standard().build();
 
-    public LambdaFunctionHandler() {}
+	public LambdaFunctionHandler() {
+	}
 
-    // Test purpose only.
-    LambdaFunctionHandler(AmazonS3 s3) {
-        this.s3 = s3;
-    }
+	// Test purpose only.
+	LambdaFunctionHandler(AmazonS3 s3) {
+		this.s3 = s3;
+	}
 
-    @Override
-    public String handleRequest(S3Event event, Context context) {
-        context.getLogger().log("Received event: " + event);
+	@Override
+	public String handleRequest(S3Event event, Context context) {
+		context.getLogger().log("Received event: " + event);
 
-        // Get the object from the event and show its content type
-        String bucket = event.getRecords().get(0).getS3().getBucket().getName();
-        String key = event.getRecords().get(0).getS3().getObject().getKey();
-        try {
-            S3Object response = s3.getObject(new GetObjectRequest(bucket, key));
-            String contentType = response.getObjectMetadata().getContentType();
-            context.getLogger().log("CONTENT TYPE: " + contentType);
-            
-            context.getLogger().log("IMAGE NAME: " + response.getKey());
-            context.getLogger().log("CONTENT: " + response.getKey());
-            ppeDetection(response.getKey(), bucket, context);
-            return contentType;
-        } catch (Exception e) {
-            e.printStackTrace();
-            context.getLogger().log(String.format(
-                "Error getting object %s from bucket %s. Make sure they exist and"
-                + " your bucket is in the same region as this function.", key, bucket));
-            throw e;
-        }
-        
-    }
-    
-    public static void ppeDetection(String photo, String bucket, Context context) {
-        AmazonRekognition rekognitionClient = AmazonRekognitionClientBuilder.defaultClient();
-        
-        ProtectiveEquipmentSummarizationAttributes summaryAttributes = new ProtectiveEquipmentSummarizationAttributes()
-                .withMinConfidence(80F)
-                .withRequiredEquipmentTypes("FACE_COVER", "HAND_COVER", "HEAD_COVER");
-                
-        DetectProtectiveEquipmentRequest request = new DetectProtectiveEquipmentRequest()
-                .withImage(new Image()
-                        .withS3Object(new com.amazonaws.services.rekognition.model.S3Object()
-                                .withName(photo).withBucket(bucket)))
-                .withSummarizationAttributes(summaryAttributes);
+		// Get the object from the event and show its content type
+		String bucket = event.getRecords().get(0).getS3().getBucket().getName();
+		String key = event.getRecords().get(0).getS3().getObject().getKey();
+		try {
+			S3Object response = s3.getObject(new GetObjectRequest(bucket, key));
+			String contentType = response.getObjectMetadata().getContentType();
+			context.getLogger().log("CONTENT TYPE: " + contentType);
 
-        try {
-        	 context.getLogger().log("Detected PPE for people in image " + photo);
-        	 context.getLogger().log("Detected people\n---------------");
-            DetectProtectiveEquipmentResult result = rekognitionClient.detectProtectiveEquipment(request);
+			context.getLogger().log("IMAGE NAME: " + response.getKey());
+			context.getLogger().log("CONTENT: " + response.getKey());
+			ppeDetection(response.getKey(), bucket, context);
+			return contentType;
+		} catch (Exception e) {
+			e.printStackTrace();
+			context.getLogger().log(String.format("Error getting object %s from bucket %s. Make sure they exist and"
+					+ " your bucket is in the same region as this function.", key, bucket));
+			throw e;
+		}
 
+	}
 
-            List <ProtectiveEquipmentPerson> persons = result.getPersons();
+	public static void ppeDetection(String photo, String bucket, Context context) {
+		AmazonRekognition rekognitionClient = AmazonRekognitionClientBuilder.defaultClient();
 
+		ProtectiveEquipmentSummarizationAttributes summaryAttributes = new ProtectiveEquipmentSummarizationAttributes()
+				.withMinConfidence(80F).withRequiredEquipmentTypes("FACE_COVER", "HAND_COVER", "HEAD_COVER");
 
-            for (ProtectiveEquipmentPerson person: persons) {
-            	 context.getLogger().log("ID: " + person.getId());
-                List<ProtectiveEquipmentBodyPart> bodyParts=person.getBodyParts();
-                if (bodyParts.isEmpty()){
-                	 context.getLogger().log("\tNo body parts detected");
-                } else
-                    for (ProtectiveEquipmentBodyPart bodyPart: bodyParts) {
-                    	 context.getLogger().log("\t" + bodyPart.getName() + ". Confidence: " + bodyPart.getConfidence().toString());
+		DetectProtectiveEquipmentRequest request = new DetectProtectiveEquipmentRequest()
+				.withImage(new Image().withS3Object(
+						new com.amazonaws.services.rekognition.model.S3Object().withName(photo).withBucket(bucket)))
+				.withSummarizationAttributes(summaryAttributes);
 
+		try {
+			String ppeDetectionResult = "";
+			ppeDetectionResult += "Detected PPE for people in image " + photo + "\n";
+			ppeDetectionResult += "Detected people\n---------------------------" + "\n";
+			DetectProtectiveEquipmentResult result = rekognitionClient.detectProtectiveEquipment(request);
 
+			List<ProtectiveEquipmentPerson> persons = result.getPersons();
 
-                        List<EquipmentDetection> equipmentDetections=bodyPart.getEquipmentDetections();
+			for (ProtectiveEquipmentPerson person : persons) {
+				ppeDetectionResult += "Detected person: " + person.getId() + "\n";
+				List<ProtectiveEquipmentBodyPart> bodyParts = person.getBodyParts();
+				if (bodyParts.isEmpty()) {
+					ppeDetectionResult += "\tNo body parts detected" + "\n";
+				} else
+					for (ProtectiveEquipmentBodyPart bodyPart : bodyParts) {
+						ppeDetectionResult += "\t" + bodyPart.getName() + ". Confidence: "
+								+ bodyPart.getConfidence().toString() + "\n";
 
-                        if (equipmentDetections.isEmpty()){
-                        	 context.getLogger().log("\t\tNo PPE Detected on " + bodyPart.getName());
+						List<EquipmentDetection> equipmentDetections = bodyPart.getEquipmentDetections();
 
-                        } 
-                        else {
-                            for (EquipmentDetection item: equipmentDetections) {
-                            	 context.getLogger().log("\t\tItem: " + item.getType() + ". Confidence: " + item.getConfidence().toString());
-                            	 context.getLogger().log("\t\tCovers body part: " 
-                                        + item.getCoversBodyPart().getValue().toString() + ". Confidence: " + item.getCoversBodyPart().getConfidence().toString());
+						if (equipmentDetections.isEmpty()) {
+							ppeDetectionResult += "\t\tNo PPE Detected on " + bodyPart.getName() + "\n";
 
-                            	 context.getLogger().log("\t\tBounding Box");
-                                BoundingBox box =item.getBoundingBox();
+						} else {
+							for (EquipmentDetection item : equipmentDetections) {
+								ppeDetectionResult += "\t\tItem: " + item.getType() + ". Confidence: "
+										+ item.getConfidence().toString() + "\n";
+								ppeDetectionResult += "\t\tCovers body part: "
+										+ item.getCoversBodyPart().getValue().toString() + ". Confidence: "
+										+ item.getCoversBodyPart().getConfidence().toString() + "\n";
 
-                                context.getLogger().log("\t\tLeft: " +box.getLeft().toString());
-                                context.getLogger().log("\t\tTop: " + box.getTop().toString());
-                                context.getLogger().log("\t\tWidth: " + box.getWidth().toString());
-                                context.getLogger().log("\t\tHeight: " + box.getHeight().toString());
-                                context.getLogger().log("\t\tConfidence: " + item.getConfidence().toString());
-                                System.out.println();
-                            }
-                        }
+								ppeDetectionResult += "\t\tBounding Box";
+								BoundingBox box = item.getBoundingBox();
 
-                    }
-            }
-            context.getLogger().log("Person ID Summary\n-----------------");
-            
-            //List<Integer> list=;
-            DisplaySummary("With required equipment", result.getSummary().getPersonsWithRequiredEquipment(),context);
-            DisplaySummary("Without required equipment", result.getSummary().getPersonsWithoutRequiredEquipment(),context);
-            DisplaySummary("Indeterminate", result.getSummary().getPersonsIndeterminate(),context);         
-       
-            
-        } catch(AmazonRekognitionException e) {
-            e.printStackTrace();
-        }
+								ppeDetectionResult += "\t\tLeft: " + box.getLeft().toString() + "\n";
+								ppeDetectionResult += "\t\tTop: " + box.getTop().toString() + "\n";
+								ppeDetectionResult += "\t\tWidth: " + box.getWidth().toString() + "\n";
+								ppeDetectionResult += "\t\tHeight: " + box.getHeight().toString() + "\n";
+								ppeDetectionResult += "\t\tConfidence: " + item.getConfidence().toString() + "\n";
+								System.out.println();
+							}
+						}
+
+					}
+			}
+			context.getLogger().log("Person ID Summary\n-----------------" + "\n");
+
+			// List<Integer> list=;
+			ppeDetectionResult += DisplaySummary("With required equipment",
+					result.getSummary().getPersonsWithRequiredEquipment(), context);
+			ppeDetectionResult += DisplaySummary("Without required equipment",
+					result.getSummary().getPersonsWithoutRequiredEquipment(), context);
+			ppeDetectionResult += DisplaySummary("Indeterminate", result.getSummary().getPersonsIndeterminate(),
+					context);
+
+			//publishToTopic(ppeDetectionResult, "arn:aws:sns:us-west-2:682086073548:ppe_detection_topic_grupo5",context);
+			String subject = "Detected PPE for people in image" + photo;
+			String bodyHTML = "<h1>Amazon SES test (AWS SDK for Java)</h1>"
+				      + "<p>This email was sent with <a href='https://aws.amazon.com/ses/'>"
+				      + "Amazon SES</a> using the <a href='https://aws.amazon.com/sdk-for-java/'>" 
+				      + "AWS SDK for Java</a>";
+			send("saraodrada@gmail.com", "juanmanuelimbachi@hotmail.com", subject, ppeDetectionResult, bodyHTML, context);
+		} catch (AmazonRekognitionException e) {
+			e.printStackTrace();
+		}
 	}
 	
-    public static void DisplaySummary(String summaryType,List<Integer> idList, Context context)
-    {
-    	 context.getLogger().log(summaryType + "\n\tIDs  ");
-        if (idList.size()==0) {
-        	 context.getLogger().log("None");
-        }
-        else {
-            int count=0;
-            for (Integer id: idList ) { 
-                if (count++ == idList.size()-1) {
-                	 context.getLogger().log(id.toString());
-                }
-                else {
-                	 context.getLogger().log(id.toString() + ", ");
-                }
-            }
-        }
-                    
-        System.out.println();
-        
-    }
-    
+	public static void send(String from, String to, String subject, String bodyText, String bodyHTML, Context context) {
+
+		AmazonSimpleEmailService client = AmazonSimpleEmailServiceClientBuilder
+				.standard()
+				.withRegion(Regions.US_WEST_2)
+				.build();
+		
+		SendEmailRequest request = new SendEmailRequest()
+				.withDestination(new Destination().withToAddresses(to))
+				.withMessage(new Message().withBody(new Body().withHtml(new Content().withCharset("UTF-8").withData(bodyHTML))
+				.withText(new Content().withCharset("UTF-8").withData(bodyText)))
+				.withSubject(new Content().withCharset("UTF-8").withData(subject)))
+				.withSource(from);
+
+		client.sendEmail(request);
+		context.getLogger().log("Email sent!");
+
+	}
+
+	public static String DisplaySummary(String summaryType, List<Integer> idList, Context context) {
+		String summary = "";
+		summary += summaryType + "\n\tIDs  " + "\n";
+
+		if (idList.size() == 0) {
+			summary += "None" + "\n";
+		} else {
+			int count = 0;
+			for (Integer id : idList) {
+				if (count++ == idList.size() - 1) {
+					summary += id.toString() + "\n";
+				} else {
+					summary += id.toString() + ", ";
+				}
+			}
+		}
+
+		summary += "\n";
+		return summary;
+
+	}
+//
+//	public static void publishToTopic(String message, String topicArn, Context context) {
+//
+//		try {
+//			SnsClient snsClient = SnsClient.builder().region(Region.US_WEST_2).build();
+//
+//			PublishRequest request = PublishRequest.builder().message(message).topicArn(topicArn).build();
+//
+//			PublishResponse result = snsClient.publish(request);
+//			context.getLogger()
+//					.log(result.messageId() + " Message sent. Status was " + result.sdkHttpResponse().statusCode());
+//			context.getLogger().log(
+//					result.messageId() + " PPE Detection results were sent " + result.sdkHttpResponse().statusCode());
+//
+//		} catch (SnsException e) {
+//			System.err.println(e.awsErrorDetails().errorMessage());
+//			System.exit(1);
+//		}
+//	}
+
 }
